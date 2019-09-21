4187c29eb3c1ffd385001eb916b475ed2ec100e3
Anton Lapshin <lapshin@amazon.com>
2019-09-21 14:58:36 +0200

    refactor(App): Adjust App container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.png';
 import './App.css';
+import { Dashboard } from './containers/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <h1>Character Search</h1>
       </header>
+      <section className="app-container">
+        <Dashboard />
+      </section>
     </div>
   );
 }
